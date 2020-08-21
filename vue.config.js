251ef944593b0ff6e4152b77a510c9c02c5ec238
Bright <lsbright@qq.com>
2020-08-21 09:53:27 +0800

    init chichen music

---
+++ vue.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+function resolve(dir) {
+    return path.join(__dirname, dir)
+}
+
+module.exports = {
+    chainWebpack: config => {
+        config.resolve.alias
+            .set('common', resolve('src/common'))
+    },
+
+    devServer: {
+        proxy: {
+            'api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^api': '/'     // 代理路径
+                }
+            }
+        }
+    }
+}
